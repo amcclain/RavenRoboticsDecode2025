745359506d723b70777062321c747707ba8fcbb4
owenlindberg <owen.lindberg6@gmail.com>
2025-10-09 10:58:00 -0700

    fixed a mistake in MainRobotCode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/MainRobotCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/MainRobotCode.java
@@ -31,8 +31,12 @@ package org.firstinspires.ftc.teamcode.opmode;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.configuration.annotations.ServoType;
+import com.qualcomm.robotcore.hardware.configuration.annotations.ServoTypes;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
@@ -40,8 +44,12 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 @TeleOp(name = "Main Robot Code", group = "Robot")
 public class MainRobotCode extends OpMode {
 
-    //declares the motors for the DriveTrain
+    //declares the motors and servos
     DcMotor frontLeftDrive, frontRightDrive, backLeftDrive, backRightDrive;
+    CRServo intake;
+    Servo LeftServo, RightServo, LiftSevro;
+
+
 
 
     //declares the Inertial Measurement Unit
@@ -53,11 +61,16 @@ public class MainRobotCode extends OpMode {
         frontRightDrive = hardwareMap.get(DcMotor.class, "front_right_motor");
         backLeftDrive = hardwareMap.get(DcMotor.class, "back_left_motor");
         backRightDrive = hardwareMap.get(DcMotor.class, "back_right_motor");
+        intake = hardwareMap.get(CRServo.class, "intake");
+        LeftServo = hardwareMap.get(Servo.class, "LeftServo");
+        RightServo = hardwareMap.get(Servo.class, "RightServo");
+        LiftSevro = hardwareMap.get(Servo.class, "LiftServo");
 
-        //flips the direction of the necessary motors
+        //flips the direction of the necessary motors and servos
         backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
         frontRightDrive.setDirection(DcMotor.Direction.REVERSE);
         backRightDrive.setDirection(DcMotor.Direction.REVERSE);
+        RightServo.setDirection(Servo.Direction.REVERSE);
 
         //tells motors to use RUN_USING_ENCODER to be more accurate
         frontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -67,8 +80,6 @@ public class MainRobotCode extends OpMode {
 
         //setting up the IMU
         imu = hardwareMap.get(IMU.class, "imu");
-
-        //tells the program the orientation of your robot
         RevHubOrientationOnRobot.LogoFacingDirection logoDirection =
                 RevHubOrientationOnRobot.LogoFacingDirection.RIGHT;
         RevHubOrientationOnRobot.UsbFacingDirection usbDirection =
@@ -81,15 +92,31 @@ public class MainRobotCode extends OpMode {
     @Override
     public void loop() {
         //add telemetry
-        telemetry.addLine("Press X to reset Yaw");
+        telemetry.addLine("Press Triangle to reset Yaw");
         telemetry.addLine("Hold left bumper to drive in robot relative");
-        telemetry.addLine("The left joystick sets the robot direction");
-        telemetry.addLine("Moving the right joystick left and right turns the robot");
+        telemetry.addLine("The left joystick moves robot");
+        telemetry.addLine("The right joystick turns the robot");
 
 
-        //X button resets robot Yaw
-        if (gamepad1.a) {
+        //Triangle button resets robot Yaw
+        if (gamepad1.y) {
             imu.resetYaw();
+        }
+
+        //X button activates intake
+        if (gamepad1.a) {
+            intake.setPower(-1);
+        } else {
+            intake.setPower(0);
+        }
+
+        //Tilt Shooter
+        if (gamepad1.x){
+            RightServo.setPosition(1);
+            LeftServo.setPosition(1);
+        } else {
+            RightServo.setPosition(0);
+            LeftServo.setPosition(0);
         }
 
 
@@ -104,7 +131,7 @@ public class MainRobotCode extends OpMode {
     }
 
 
-    //Field relative driving
+    //Field relative drive
     private void driveFieldRelative(double forward, double right, double rotate) {
         // First, convert direction being asked to drive to polar coordinates
         double theta = Math.atan2(forward, right);
@@ -122,7 +149,7 @@ public class MainRobotCode extends OpMode {
         drive(newForward, newRight, rotate);
     }
 
-    //traditional driving
+    //traditional drive
     public void drive(double forward, double right, double rotate) {
         // This calculates the power needed for each wheel based on the amount of forward,
         // strafe right, and rotate
