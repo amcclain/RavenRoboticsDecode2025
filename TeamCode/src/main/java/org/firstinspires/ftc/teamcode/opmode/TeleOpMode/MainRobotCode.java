36e4d03078064d8a444ecd6cb919bf45ee53715d
owenlindberg <owen.lindberg6@gmail.com>
2025-12-13 17:23:22 -0800

    deleted comments in initAprilTag

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TeleOpMode/MainRobotCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TeleOpMode/MainRobotCode.java
@@ -311,20 +311,6 @@ public class MainRobotCode extends OpMode {
         // Create the AprilTag processor.
         aprilTag = new AprilTagProcessor.Builder()
 
-                // The following default settings are available to un-comment and edit as needed.
-                //.setDrawAxes(false)
-                //.setDrawCubeProjection(false)
-                //.setDrawTagOutline(true)
-                //.setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
-                //.setTagLibrary(AprilTagGameDatabase.getCenterStageTagLibrary())
-                //.setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
-
-                // == CAMERA CALIBRATION ==
-                // If you do not manually specify calibration parameters, the SDK will attempt
-                // to load a predefined calibration for your camera.
-                //.setLensIntrinsics(578.272, 578.272, 402.145, 221.506)
-                // ... these parameters are fx, fy, cx, cy.
-
                 .build();
 
         // Create the vision portal by using a builder.
@@ -341,20 +327,8 @@ public class MainRobotCode extends OpMode {
             throw new IllegalArgumentException("Camera Not Working");
         }
 
-
-        // Choose a camera resolution. Not all cameras support all resolutions.
-        //builder.setCameraResolution(new Size(640, 480));
-
         // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
         builder.enableLiveView(true);
-
-        // Set the stream format; MJPEG uses less bandwidth than default YUY2.
-        //builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
-
-        // Choose whether or not LiveView stops if no processors are enabled.
-        // If set "true", monitor shows solid orange screen if no processors enabled.
-        // If set "false", monitor shows camera view without annotations.
-        //builder.setAutoStopLiveView(false);
 
         // Set and enable the processor.
         builder.addProcessor(aprilTag);
