ac9dbd52475ebddff13c5adbb91107fa36eed36d
owenlindberg <owen.lindberg6@gmail.com>
2025-12-18 14:11:14 -0800

    started of making a LED controller

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/Concepts/ConceptBlinkinLEDs.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.opmode.Concepts;
+
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.util.WaverlyGamepad;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@TeleOp(name = "LEDs", group = "Robot")
+public class ConceptBlinkinLEDs extends OpMode {
+
+    RevBlinkinLedDriver led;
+    WaverlyGamepad gp = null;
+    List<RevBlinkinLedDriver.BlinkinPattern> list = new ArrayList<>();
+
+    @Override
+    public void init() {
+        led = hardwareMap.get(RevBlinkinLedDriver.class, "LED");
+        gp = new WaverlyGamepad(gamepad1);
+
+        list.add(RevBlinkinLedDriver.BlinkinPattern.RED);
+        list.add(RevBlinkinLedDriver.BlinkinPattern.ORANGE);
+        list.add(RevBlinkinLedDriver.BlinkinPattern.YELLOW);
+        list.add(RevBlinkinLedDriver.BlinkinPattern.LIME);
+        list.add(RevBlinkinLedDriver.BlinkinPattern.GREEN);
+        list.add(RevBlinkinLedDriver.BlinkinPattern.DARK_GREEN);
+        list.add(RevBlinkinLedDriver.BlinkinPattern.BLUE);
+        list.add(RevBlinkinLedDriver.BlinkinPattern.DARK_BLUE);
+        list.add(RevBlinkinLedDriver.BlinkinPattern.BLUE_VIOLET);
+        list.add(RevBlinkinLedDriver.BlinkinPattern.VIOLET);
+    }
+
+
+    @Override
+    public void loop() {
+        gp.readButtons();
+
+
+    }
+}
